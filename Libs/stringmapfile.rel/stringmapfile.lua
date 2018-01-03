30cb9122aba88e0658c6cfa16bb579e3e0b8b47d
Tricky1975 <facebook@tbbs.nl>
2018-01-03 21:38:31 +0100

    String Map File

---
+++ Libs/stringmapfile.rel/stringmapfile.lua
@@ -0,0 +1,53 @@
+--[[
+  stringmapfile.lua
+  
+  version: 18.01.03
+  Copyright (C) 2017, 2018 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+
+-- These tags were used in the old love builder, but this has now been re set up for Ryanna.
+-- BuildLove -- *import binread
+-- BuildLove -- *import qhs 
+
+function readstringmap(file)
+    local bt=binread(file)
+    local ret = {}
+    local tag,factor,k,v
+    repeat
+        if bt:eof() then return ret end -- please note closure is not required!
+        local tag = bt:getbyte()
+        if tag==255 then return ret end
+        if tag==2 then 
+           factor=bt:getbyte()
+           k = QUH(bt:getstring())
+           v = QUH(bt:getstring())
+           ret[k]=v
+        elseif tag==1 then
+           k = bt:getstring()
+           v = bt:getstring()
+           ret[k]=v
+        else
+           error("Unknown tag in readstringmap.\nFile: "..file.."\bTag:  "..tag)
+        end
+    until false    
+end
+
+--[[
+mkl.version("Ryanna Libraries - stringmapfile.lua","18.01.03")
+mkl.lic    ("Ryanna Libraries - stringmapfile.lua","ZLib License")
+]]
+
+return true
