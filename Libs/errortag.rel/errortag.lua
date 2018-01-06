0498ef8e7b6e317f7fe0506e1b19c7ca8520187a
Tricky1975 <facebook@tbbs.nl>
2018-01-06 18:35:42 +0100

    errortag

---
+++ Libs/errortag.rel/errortag.lua
@@ -0,0 +1,40 @@
+--[[
+  errortag.lua
+  
+  version: 18.01.06
+  Copyright (C) 2018 2017
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+
+return function(fn,a,message)
+   local ret = fn .."("    
+   for i,item in ipairs(a) do
+       if i~=1 then ret = ret ..", " end
+       if type(item)=="string" then
+          ret = ret .. '"'..replace(item,'"',"\\\"")..'"'
+       elseif type(item)=="number" then
+          ret = ret .. item
+       elseif type(item)=="booleitemn" then
+          if item then ret = ret .."true" else ret = ret .. "fitemlse" end
+       else
+          ret = ret .. "<<"..type(a)..">>"
+       end
+    end   
+    ret = ret .. "): "..message
+    return ret    
+end          
+          
+          
+   
