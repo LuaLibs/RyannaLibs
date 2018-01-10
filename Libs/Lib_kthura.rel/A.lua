39255b5d7c23d2544208562c143c9caefc467c8d
Tricky1975 <facebook@tbbs.nl>
2018-01-10 12:29:33 +0100

    Link to the pathfinder

--- Libs/Lib_kthura.rel/A.lua
+++ Libs/Lib_kthura.rel/A.lua
@@ -6,8 +6,11 @@
 	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
 	distributed with this file, You can obtain one at 
 	http://mozilla.org/MPL/2.0/.
-        Version: 18.01.05
+        Version: 18.01.10
 ]]
+
+-- $USE libs/as_pathfinder
+
 kthura = {}
 
 return true
