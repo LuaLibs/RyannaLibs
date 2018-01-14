4543b3ed75add06fca7a4c82b18da08232d02831
Tricky1975 <facebook@tbbs.nl>
2018-01-14 22:19:13 +0100

    Various issues fixed causing bugs in loading portraits

--- Libs/Scenario.rel/Scenario.lua
+++ Libs/Scenario.rel/Scenario.lua
@@ -17,25 +17,6 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 ]]
---[[
-  Scenario.lua
-  
-  version: 17.07.16
-  Copyright (C) 2016, 2017 Jeroen P. Broks
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-]]
 
 -- $USE libs/qgfx
 
@@ -115,13 +96,16 @@ for LineNumber,Line in ipairs(crap) do
 local k,i,tag,rec
 local picfile,picref
 for k,tag in pairs(ret) do for i,rec in pairs(tag) do
-     picfile = "GFX/Boxtext/Portret/"..sval(rec.PicDir).."/"..sval(rec.PicSpc)..".png"
+     picfile = "GFX/Portret/"..sval(rec.PicDir).."/"..sval(rec.PicSpc)..".png"
+     --print('picfile',picfile)
      picref = upper(rec.PicDir).."."..upper(rec.PicSpc)
-     if Image.Exist(picref)==0 and JCR6.Exists(picfile)==1 then 
-        Image.AssignLoad(picref,picfile) 
+     if (not ImageLoaded(picref)) and JCR_Exists(picfile) then 
+        local ti = Image.AssignLoad(picfile,picref) 
         portret[picref]=true 
         CSay('Loaded '..picfile..' on '..picref.." (BoxText)") 
-     elseif Image.Exist(picref)~=0 then
+        if not ImageLoaded(picref) then CSay("= FAILED!",255,0,0) end
+        if not ti then CSay("= NIL RETURNED",255,0,0) end
+     elseif Image.Exist(picref) then
         portret[picref]=true
         end
      if portret[picref] then rec.PicRef=picref; end 
