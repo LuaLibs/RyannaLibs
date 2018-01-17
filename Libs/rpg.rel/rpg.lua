c7539cc24dfd9f13d4c12b0efc68b8c0cfed3d64
Tricky1975 <facebook@tbbs.nl>
2018-01-17 10:12:22 +0100

    Fixed PhantasarProductions/63fires#21

--- Libs/rpg.rel/rpg.lua
+++ Libs/rpg.rel/rpg.lua
@@ -1,7 +1,7 @@
 --[[
   rpg.lua
   
-  version: 18.01.16
+  version: 18.01.17
   Copyright (C) 2018 Jeroen P. Broks
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
